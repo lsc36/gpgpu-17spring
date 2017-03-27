4ed7ecc6c75ed82a2c5d71f3721fba96a1e336b0
Shao-Chuan Lee <lsc@lv6.tw>
2017-03-27 19:09:30 +0800

    lab1: ver. 1

--- lab1/lab1.cu
+++ lab1/lab1.cu
@@ -1,35 +1,105 @@
 #include "lab1.h"
 
+#include <cstdio>
+#include <cassert>
+#include <sndfile.h>
+
+#include "SyncedMemory.h"
+
 namespace {
 
-const unsigned W = 640;
-const unsigned H = 480;
-const unsigned NFRAME = 240;
+const unsigned W = 640, H = 480, FPS = 24;
+const char *AUDIO_FILE = "audio.wav";
+const unsigned SAMPLE_RATE = 44100, CHANNELS = 2;
+const unsigned WINDOW_SIZE = SAMPLE_RATE / FPS;
+const unsigned BLOCKDIM_X = 32, BLOCKDIM_Y = 24;
 
 }  // namespace
 
+namespace kernel {
+
+__global__ void GetAudioWindow(const float *audio, unsigned offset,
+		float *window) {
+	const unsigned x = blockIdx.x * blockDim.x + threadIdx.x;
+	if (!(x < WINDOW_SIZE))
+		return;
+
+	window[x] = (audio[offset + 2*x] + audio[offset + 2*x + 1]) / 2;
+}
+
+__global__ void GenerateFrame(const float *window, uint8_t *yuv) {
+	const unsigned x = blockIdx.x * blockDim.x + threadIdx.x;
+	const unsigned y = blockIdx.y * blockDim.y + threadIdx.y;
+	if (!(x < W && y < H))
+		return;
+
+	size_t off = x * WINDOW_SIZE / W;
+	if (abs(window[off] - (2.0 * y / (H-1) - 1)) < 0.02)
+		yuv[y*W + x] = 255;
+	else
+		yuv[y*W + x] = 0;
+
+	if (x % 2 == 0 && y % 2 == 0) {
+		yuv[W*H + (y/2)*(W/2) + x/2] = 128;
+		yuv[W*H + (W/2)*(H/2) + (y/2)*(W/2) + x/2] = 128;
+	}
+}
+
+}  // namespace kernel
+
 class Lab1VideoGenerator::Impl {
 public:
-	Impl(): t(0) {}
+	Impl();
 	void get_info(Lab1VideoInfo&);
 	void Generate(uint8_t *yuv);
 private:
-	int t;
+	MemoryBuffer<float> m_buffer;
+	SyncedMemory<float> m_audioMem, m_windowMem;
+	unsigned m_numFrames;
+	unsigned m_curFrame;
 };
+
+Lab1VideoGenerator::Impl::Impl() {
+	SF_INFO sfinfo;
+	SNDFILE *sndfile = sf_open(AUDIO_FILE, SFM_READ, &sfinfo);
+	assert(sfinfo.samplerate == SAMPLE_RATE);
+	assert(sfinfo.channels == CHANNELS);
+	m_numFrames = sfinfo.frames * FPS / SAMPLE_RATE;
+
+	printf("numFrames = %u, length = %u:%u.%u\n",
+			m_numFrames, m_numFrames / FPS / 60,
+			m_numFrames / FPS % 60, m_numFrames % FPS);
+
+	m_buffer.Realloc(sfinfo.frames * sfinfo.channels + WINDOW_SIZE);
+	m_audioMem = m_buffer.CreateSync(sfinfo.frames * CHANNELS);
+	m_windowMem = m_buffer.CreateSync(WINDOW_SIZE,
+			sfinfo.frames * CHANNELS);
+
+	assert(sf_readf_float(sndfile, m_audioMem.get_cpu_wo(), sfinfo.frames)
+			== sfinfo.frames);
+	sf_close(sndfile);
+}
 
 void Lab1VideoGenerator::Impl::get_info(Lab1VideoInfo &info) {
 	info.w = W;
 	info.h = H;
-	info.n_frame = NFRAME;
-	// fps = 24/1 = 24
-	info.fps_n = 24;
+	info.n_frame = m_numFrames;
+	info.fps_n = FPS;
 	info.fps_d = 1;
 }
 
 void Lab1VideoGenerator::Impl::Generate(uint8_t *yuv) {
-	cudaMemset(yuv, t*255/NFRAME, W*H);
-	cudaMemset(yuv+W*H, 128, W*H/2);
-	++t;
+	assert(m_curFrame < m_numFrames);
+	kernel::GetAudioWindow<<<
+		(WINDOW_SIZE - 1)/BLOCKDIM_X + 1,
+		BLOCKDIM_X>>>(
+			m_audioMem.get_gpu_ro(),
+			m_curFrame * SAMPLE_RATE * CHANNELS / FPS,
+			m_windowMem.get_gpu_wo());
+	kernel::GenerateFrame<<<
+		dim3((W - 1)/BLOCKDIM_X + 1, (H - 1)/BLOCKDIM_Y + 1),
+		dim3(BLOCKDIM_X, BLOCKDIM_Y)>>>(m_windowMem.get_gpu_ro(), yuv);
+	m_curFrame++;
 }
 
 Lab1VideoGenerator::Lab1VideoGenerator(): impl(new Impl) {}
