2d55b31a302de70b069cd7c5e61c12bbaffe049b
Shao-Chuan Lee <lsc@lv6.tw>
2017-05-04 20:41:09 +0800

    lab2: CountPosition1

--- lab2/counting.cu
+++ lab2/counting.cu
@@ -10,8 +10,25 @@
 __device__ __host__ int CeilDiv(int a, int b) { return (a-1)/b + 1; }
 __device__ __host__ int CeilAlign(int a, int b) { return CeilDiv(a, b) * b; }
 
+struct MyUnaryOp {
+    __device__ int operator()(const char &c) const {
+        return c == '\n' ? 0 : 1;
+    }
+};
+
+struct MyBinaryPred {
+    __device__ bool operator()(const char &a, const char &b) const {
+        return b != '\n';
+    }
+};
+
 void CountPosition1(const char *text, int *pos, int text_size)
 {
+    thrust::transform(thrust::system::cuda::par,
+                      text, text + text_size, pos, MyUnaryOp());
+    thrust::inclusive_scan_by_key(thrust::system::cuda::par,
+                                  text, text + text_size, pos, pos,
+                                  MyBinaryPred());
 }
 
 void CountPosition2(const char *text, int *pos, int text_size)
