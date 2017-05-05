b94f1b03caf855633c2a00b4c7813df5787589a4
Shao-Chuan Lee <lsc@lv6.tw>
2017-05-05 22:17:30 +0800

    lab2: CountPosition2 ver. 0.2

--- lab2/counting.cu
+++ lab2/counting.cu
@@ -31,37 +31,102 @@ void CountPosition1(const char *text, int *pos, int text_size)
                                   MyBinaryPred());
 }
 
-__global__ void MySegmentedScanInit(const char *text, int *pos1, int *pos2,
-                                    int text_size)
+__device__ void MySegmentedScanInit(const char *text, int *pos, int text_size)
 {
     const int i = blockIdx.x * blockDim.x + threadIdx.x;
+
     if (i < text_size)
-        pos1[i] = pos2[i] = text[i] == '\n' ? 0 : 1;
+        pos[i] = text[i] == '\n' ? 0 : 1;
+    else
+        pos[i] = 0;
+
+    __syncthreads();
 }
 
-__global__ void MySegmentedScanPass(int *pos1, const int *pos2, int text_size,
-                                    int offset)
+__device__ void MySegmentedScanIntraBlock(int *pos)
 {
-    const int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < text_size - offset && pos1[i + offset] == offset)
-        pos1[i + offset] += pos2[i];
+    const int tid = threadIdx.x;
+    int tmp;
+
+    if (tid >=   1) {
+	tmp = pos[tid -   1]; __syncthreads();
+	if (pos[tid] ==   1) pos[tid] += tmp;
+    }
+    __syncthreads();
+    if (tid >=   2) {
+	tmp = pos[tid -   2]; __syncthreads();
+	if (pos[tid] ==   2) pos[tid] += tmp;
+    }
+    __syncthreads();
+    if (tid >=   4) {
+	tmp = pos[tid -   4]; __syncthreads();
+	if (pos[tid] ==   4) pos[tid] += tmp;
+    }
+    __syncthreads();
+    if (tid >=   8) {
+	tmp = pos[tid -   8]; __syncthreads();
+	if (pos[tid] ==   8) pos[tid] += tmp;
+    }
+    __syncthreads();
+    if (tid >=  16) {
+	tmp = pos[tid -  16]; __syncthreads();
+	if (pos[tid] ==  16) pos[tid] += tmp;
+    }
+    __syncthreads();
+    if (tid >=  32) {
+	tmp = pos[tid -  32]; __syncthreads();
+	if (pos[tid] ==  32) pos[tid] += tmp;
+    }
+    __syncthreads();
+    if (tid >=  64) {
+	tmp = pos[tid -  64]; __syncthreads();
+	if (pos[tid] ==  64) pos[tid] += tmp;
+    }
+    __syncthreads();
+    if (tid >= 128) {
+	tmp = pos[tid - 128]; __syncthreads();
+	if (pos[tid] == 128) pos[tid] += tmp;
+    }
+    __syncthreads();
+    if (tid >= 256) {
+	tmp = pos[tid - 256]; __syncthreads();
+	if (pos[tid] == 256) pos[tid] += tmp;
+    }
+    __syncthreads();
 }
 
-const int BS = 128;
+__device__ void MySegmentedScanMergeBlocks(int *pos)
+{
+    const int tid = threadIdx.x;
+
+    if (pos[tid] == tid + 1) pos[tid] += pos[-1];
+}
+
+__global__ void MySegmentedScanStep1(const char *text, int *pos, int text_size)
+{
+    const int bid = blockIdx.x;
+
+    MySegmentedScanInit(text, pos, text_size);
+    MySegmentedScanIntraBlock(pos + 512 * bid);
+}
+
+__global__ void MySegmentedScanStep2(int *pos)
+{
+    const int bid = blockIdx.x;
+
+    MySegmentedScanMergeBlocks(pos + 512 * (bid + 1));
+}
 
 void CountPosition2(const char *text, int *pos, int text_size)
 {
     int *pos2 = nullptr;
-    cudaMalloc(&pos2, text_size * sizeof(int));
+    const int size = CeilAlign(text_size, 512);
+    cudaMalloc(&pos2, size * sizeof(int));
 
-    MySegmentedScanInit<<<CeilDiv(text_size, BS), BS>>>(
-            text, pos, pos2, text_size);
-    for (int offset = 1; offset < text_size; offset <<= 1) {
-        MySegmentedScanPass<<<CeilDiv(text_size, BS), BS>>>(
-                pos, pos2, text_size, offset);
-        cudaMemcpy(pos2, pos, text_size * sizeof(int),
-                   cudaMemcpyDeviceToDevice);
-    }
+    MySegmentedScanStep1<<<size / 512, 512>>>(text, pos2, text_size);
+    cudaDeviceSynchronize();
+    MySegmentedScanStep2<<<size / 512 - 1, 512>>>(pos2);
 
+    cudaMemcpy(pos, pos2, text_size * sizeof(int), cudaMemcpyDeviceToDevice);
     cudaFree(pos2);
 }
