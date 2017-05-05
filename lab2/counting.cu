4a36b7f92bd0b3b64808c244a6474d34a54d543d
Shao-Chuan Lee <lsc@lv6.tw>
2017-05-05 12:46:49 +0800

    lab2: CountPosition2 ver. 0.1

--- lab2/counting.cu
+++ lab2/counting.cu
@@ -31,6 +31,37 @@ void CountPosition1(const char *text, int *pos, int text_size)
                                   MyBinaryPred());
 }
 
+__global__ void MySegmentedScanInit(const char *text, int *pos1, int *pos2,
+                                    int text_size)
+{
+    const int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < text_size)
+        pos1[i] = pos2[i] = text[i] == '\n' ? 0 : 1;
+}
+
+__global__ void MySegmentedScanPass(int *pos1, const int *pos2, int text_size,
+                                    int offset)
+{
+    const int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < text_size - offset && pos1[i + offset] == offset)
+        pos1[i + offset] += pos2[i];
+}
+
+const int BS = 128;
+
 void CountPosition2(const char *text, int *pos, int text_size)
 {
+    int *pos2 = nullptr;
+    cudaMalloc(&pos2, text_size * sizeof(int));
+
+    MySegmentedScanInit<<<CeilDiv(text_size, BS), BS>>>(
+            text, pos, pos2, text_size);
+    for (int offset = 1; offset < text_size; offset <<= 1) {
+        MySegmentedScanPass<<<CeilDiv(text_size, BS), BS>>>(
+                pos, pos2, text_size, offset);
+        cudaMemcpy(pos2, pos, text_size * sizeof(int),
+                   cudaMemcpyDeviceToDevice);
+    }
+
+    cudaFree(pos2);
 }
